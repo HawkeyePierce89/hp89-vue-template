fced259e13ce43a4d573a886a3293ee3cb737b97
Anton Karmanov <anton@karmanov.ws>
2018-04-22 18:55:48 +0700

    [ADD] files

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+
+module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    filename: 'build.js',
+    historyApiFallback: true,
+  },
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    publicPath: '/',
+    filename: 'build.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            css: 'style-loader!css-loader!less-loader',
+          },
+        },
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.less$/,
+        loader: 'style-loader!css-loader!less-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.runtime.min.js',
+    },
+    modules: [
+      path.resolve('./src'),
+      path.resolve('./node_modules'),
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    inject: "body",
+    template: 'src/index.html',
+  })]
+};
